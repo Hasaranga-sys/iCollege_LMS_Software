67f721384b1c5f957d23223ac373bbd85125b13c
udulaTharupathi99 <udulatharupathi333@gmail.com>
2022-09-08 04:23:47 +0530

    Create add library item UI

--- front-end/src/Components/Admin/ViewLibraryItems.jsx
+++ front-end/src/Components/Admin/ViewLibraryItems.jsx
@@ -1,6 +1,27 @@
 import React from 'react'
+import { useState,useEffect } from 'react'
 
 function ViewLibraryItems() {
+  const [itemList, setItemList] = useState([]);
+
+  useEffect(() => {
+    console.log("hi");
+  
+    
+  }, [])
+
+  const updateClicked = (id) => {
+    console.log(id);
+    //navigate(`/admin-rooms/${id}`);
+  };
+
+  const deleteClicked = (id) => {
+    // alert(id);
+    //Swal.fire(" succesfully deleted");
+    
+  };
+  
+
   return (
     <div>ViewLibraryItems</div>
   )
