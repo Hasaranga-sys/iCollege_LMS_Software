53c843629eb31464c7ca1c1959efc034a5da2d4f
dilshanlahiru <lahiru.kariyawasam123@gmail.com>
2022-10-13 14:58:54 +0530

    serch in user list

--- front-end/src/Components/Admin/AdminHome.jsx
+++ front-end/src/Components/Admin/AdminHome.jsx
@@ -1,62 +1,84 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const AdminHome = () => {
   return (
-    
-    <div className="card w-75 mx-auto mt-5 bg-light p-5" style={{borderRadius:30}} >
+    <div
+      className="card w-75 mx-auto mt-5 bg-light p-5"
+      style={{ borderRadius: 30 }}
+    >
+      <div className=" card border ">
+        <h2 style={{ borderRadius: 30, textAlign: "center" }}>
+          Admin Dashboard
+        </h2>
 
-<div className=' card border '>
-    <h2 style={{borderRadius:30, textAlign:"center"}} >Admin Dashboard</h2>
-    
+        <div class="py-5 ">
+          <div class="container">
+            <div class="row mx-auto ">
+              <div class="col-md-3 mx-auto">
+                <div style={{ height: 190 }} class="card text-center shadow-lg">
+                  <div class="card-block">
+                    <img
+                      style={{ height: 130, width: 130 }}
+                      class="card-img-top"
+                      src="https://cdn-icons-png.flaticon.com/512/2967/2967357.png"
+                      alt="Card image cap"
+                    />
+                    <h4 class="card-title"></h4>
+                    <Link
+                      className="btn btn-primary"
+                      to={"/AdminHome/NoticeTable"}
+                    >
+                      Announcements
+                    </Link>
+                  </div>
+                </div>
+              </div>
 
-<div class="py-5 ">
-  <div class="container">
-    <div class="row mx-auto ">
+              <div class="col-md-3 mx-auto">
+                <div style={{ height: 190 }} class="card text-center shadow-lg">
+                  <div class="card-block">
+                    <img
+                      style={{ height: 130, width: 130 }}
+                      class="card-img-top"
+                      src="https://cdn-icons-png.flaticon.com/512/2641/2641310.png"
+                      alt="Card image cap"
+                    />
+                    <h4 class="card-title"></h4>
+                    <Link
+                      className="btn btn-primary"
+                      to={"/AdminHome/ViewLibararyItems"}
+                    >
+                      E-Library
+                    </Link>
+                  </div>
+                </div>
+              </div>
 
-    <div class="col-md-3 mx-auto">
-        <div style={{height: 190}} class="card text-center shadow-lg">
-          <div class="card-block">
-          <img style={{height: 130, width: 130}} class="card-img-top" src="https://cdn-icons-png.flaticon.com/512/2967/2967357.png" alt="Card image cap"/>
-            <h4 class="card-title"></h4>
-            <Link className='btn btn-primary' to={"/AdminHome/NoticeTable"}>Announcements</Link>       
+              <div class="col-md-3 mx-auto ">
+                <div style={{ height: 190 }} class="card text-center shadow-lg">
+                  <div class="card-block">
+                    <img
+                      style={{ height: 130, width: 130 }}
+                      class="card-img-top"
+                      src="https://cdn-icons-png.flaticon.com/512/476/476863.png"
+                      alt="Card image cap"
+                    />
+                    <h4 class="card-title"></h4>
+                    <Link className="btn btn-primary" to={"/users"}>
+                      User Management
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <br></br>
           </div>
         </div>
       </div>
-
-      <div class="col-md-3 mx-auto">
-        <div style={{height: 190}} class="card text-center shadow-lg">
-          <div class="card-block">
-          <img style={{height: 130, width: 130}} class="card-img-top" src="https://cdn-icons-png.flaticon.com/512/2641/2641310.png" alt="Card image cap"/>
-            <h4 class="card-title"></h4>
-            <Link className='btn btn-primary' to={"/AdminHome/ViewLibararyItems"}>E-Library</Link>       
-          </div>
-        </div>
-      </div>
-
-      <div class="col-md-3 mx-auto ">
-        <div style={{height: 190}} class="card text-center shadow-lg">
-          <div class="card-block">
-          <img style={{height: 130, width: 130}} class="card-img-top" src="https://cdn-icons-png.flaticon.com/512/476/476863.png" alt="Card image cap"/>
-            <h4 class="card-title"></h4>
-            <Link className='btn btn-primary' to={"/AdminHome/NoticeTable"}>User Management</Link>       
-          </div>
-        </div>
-      </div>
-
-
     </div>
+  );
+};
 
-    <br></br>
-  </div>
-</div>
-
-
-  </div>        
-
-    </div>
-    
-  )
-}
-
-export default AdminHome
+export default AdminHome;
--- front-end/src/Components/UserManagement/UserList.jsx
+++ front-end/src/Components/UserManagement/UserList.jsx
@@ -5,6 +5,9 @@ import UserServices from "../Service/UserServices";
 
 const UserList = () => {
   const [user, setUser] = useState([]);
+  const [search, setSearch] = useState("");
+  const [checked, setChecked] = React.useState(false);
+  const [adminFilter, setAdminFilter] = useState("");
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -26,63 +29,207 @@ const UserList = () => {
     console.log(id);
   };
   return (
-    <div className="container">
-      <br />
-      <br />
-      <div className="card-body">
-        {/* <button onClick={AddStudent}>Add student</button> */}
-        <h1>User list</h1>
-        <br />
-        <br />
-        <table class="table table-hover">
-          <thead>
-            <tr>
-              <th scope="col"> Reg Number</th>
-              <th scope="col">Last Name</th>
-              <th scope="col">Initials</th>
-              <th scope="col">Mobile Number</th>
-              <th scope="col">Email</th>
-              <th scope="col">Department</th>
-              <th scope="col"> Role</th>
-              <th scope="col">Update</th>
-              <th scope="col">Remove</th>
-            </tr>
-          </thead>
-          <tbody>
-            {user.map((user) => (
-              <tr key={user._id}>
-                <td>{user.regNumber}</td>
-                <td>{user.lastName}</td>
-                <td>{user.initials}</td>
-                <td>{user.mobileNumber}</td>
-                <td>{user.email}</td>
-                <td>{user.faculty}</td>
-                <td>{user.role}</td>
+    // <div className="container">
+    //   <br />
+    //   <br />
+    //   <div className="card-body">
+    //     {/* <button onClick={AddStudent}>Add student</button> */}
+    //     <h1>User list</h1>
+    //     <br />
+    //     <br />
+    //     <table class="table table-hover">
+    //       <thead>
+    //         <tr>
+    //           <th scope="col"> Reg Number</th>
+    //           <th scope="col">Last Name</th>
+    //           <th scope="col">Initials</th>
+    //           <th scope="col">Mobile Number</th>
+    //           <th scope="col">Email</th>
+    //           <th scope="col">Department</th>
+    //           <th scope="col"> Role</th>
+    //           <th scope="col">Update</th>
+    //           <th scope="col">Remove</th>
+    //         </tr>
+    //       </thead>
+    //       <tbody>
+    //         {user.map((user) => (
+    //           <tr key={user._id}>
+    //             <td>{user.regNumber}</td>
+    //             <td>{user.lastName}</td>
+    //             <td>{user.initials}</td>
+    //             <td>{user.mobileNumber}</td>
+    //             <td>{user.email}</td>
+    //             <td>{user.faculty}</td>
+    //             <td>{user.role}</td>
 
-                <td>
-                  <button
-                    className="btn btn-warning"
-                    onClick={() => updteClicked(user._id)}
-                  >
-                    Update
-                  </button>
-                </td>
-                <td>
-                  <button
-                    className="btn btn-success"
-                    onClick={() => dleteClicked(user._id)}
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <div id="myDIV">
-          <button className="btn btn-primary" onClick={AddStudent}>
-            Add User
-          </button>
+    //             <td>
+    //               <button
+    //                 className="btn btn-warning"
+    //                 onClick={() => updteClicked(user._id)}
+    //               >
+    //                 Update
+    //               </button>
+    //             </td>
+    //             <td>
+    //               <button
+    //                 className="btn btn-success"
+    //                 onClick={() => dleteClicked(user._id)}
+    //               >
+    //                 Delete
+    //               </button>
+    //             </td>
+    //           </tr>
+    //         ))}
+    //       </tbody>
+    //     </table>
+    //     <div id="myDIV">
+    //       <button className="btn btn-primary" onClick={AddStudent}>
+    //         Add User
+    //       </button>
+    //     </div>
+    //   </div>
+    // </div>
+    <div className="p-3">
+      <div
+        className="shadow card w-100 text-center p-3 mt-5 mb-5"
+        style={{ borderRadius: 30 }}
+      >
+        <h1>User List</h1>
+
+        <div>
+          <div className="container p-1 mt-4 mb-4">
+            <div className="row">
+              <div className="shadow card mx-auto w-100">
+                <br></br>
+                <div>
+                  <input
+                    type="text"
+                    placeholder="search"
+                    className="form-control"
+                    style={{
+                      marginLeft: "1%",
+                      marginTop: 20,
+                      marginBottom: 20,
+                      width: "40%",
+                    }}
+                    onChange={(e) => {
+                      setSearch(e.target.value);
+                    }}
+                  />
+                </div>
+
+                <table class="table table-striped mt-3">
+                  <thead className="table-primary">
+                    <tr>
+                      <th scope="col"> Reg Number</th>
+                      <th scope="col">Last Name</th>
+                      <th scope="col">Initials</th>
+                      <th scope="col">Mobile Number</th>
+                      <th scope="col">Email</th>
+                      <th scope="col">Department</th>
+                      <th scope="col"> Role</th>
+                      <th scope="col">Update</th>
+                      <th scope="col">Remove</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {user
+                      .filter((val) => {
+                        // if (
+                        //   search === "" &&
+                        //   val.role.toLowerCase().includes(adminFilter)
+                        // ) {
+                        //   // return val;
+                        if (search === "") {
+                          return val;
+                        } else if (
+                          search === "" &&
+                          val.role.toLowerCase().includes(adminFilter)
+                        ) {
+                          return val;
+                        } else if (
+                          val.regNumber
+                            .toLowerCase()
+                            .includes(search.toLowerCase()) ||
+                          val.role
+                            .toLowerCase()
+                            .includes(search.toLowerCase()) ||
+                          val.lastName
+                            .toLowerCase()
+                            .includes(search.toLowerCase()) ||
+                          val.faculty
+                            .toLowerCase()
+                            .includes(search.toLowerCase())
+                        ) {
+                          console.log(val);
+                          return val;
+                        }
+                        return 0;
+                      })
+
+                      .map((user) => (
+                        <tr key={user._id}>
+                          <td>{user.regNumber}</td>
+                          <td>{user.lastName}</td>
+                          <td>{user.initials}</td>
+                          <td>{user.mobileNumber}</td>
+                          <td>{user.email}</td>
+                          <td>{user.faculty}</td>
+                          <td>{user.role}</td>
+
+                          <td>
+                            <button
+                              className="btn btn-info"
+                              onClick={() => updteClicked(user._id)}
+                            >
+                              Update
+                            </button>
+                          </td>
+                          <td>
+                            <button
+                              className="btn btn-danger"
+                              onClick={() => dleteClicked(user._id)}
+                            >
+                              Delete
+                            </button>
+                          </td>
+                        </tr>
+                      ))}
+                  </tbody>
+                </table>
+
+                {/* <div>
+                  <label>
+                    <input
+                      type="checkbox"
+                      checked={checked}
+                      onChange={() => {
+                        setChecked(!checked);
+                        setAdminFilter("admin");
+                        setTimeout(function () {
+                          if (checked.toString() != "true") {
+                            console.log(adminFilter);
+                          }
+                          console.log("Executed after 1 second");
+                        }, 1000);
+                      }}
+                    />
+                    My Value (admin)
+                  </label>
+
+                  <p>Is "My Value" checked? {checked.toString()}</p>
+                </div> */}
+
+                <button
+                  className="btn btn-primary w-25 mt-3"
+                  onClick={AddStudent}
+                >
+                  Add User
+                </button>
+                <br></br>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
