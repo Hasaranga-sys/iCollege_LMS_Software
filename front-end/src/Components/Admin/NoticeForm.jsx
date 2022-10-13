bd076ffb78e0acb07892eb5f818b4cec732f676b
tharaka-99 <tharakaudawatta1@gmail.com>
2022-10-13 23:40:34 +0530

    lecture update function

--- front-end/src/Components/Admin/NoticeForm.jsx
+++ front-end/src/Components/Admin/NoticeForm.jsx
@@ -1,91 +1,109 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
-import NoticeService from '../Service/NoticeService'
-import Calendar from 'react-calendar'
-import 'react-calendar/dist/Calendar.css';
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import NoticeService from "../Service/NoticeService";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 import Swal from "sweetalert2";
 const NoticeForm = () => {
-    const [faculty, setFaculty] = useState([])
-    const [date,setDate] = useState("")
-    const [topic,setTopic] = useState("")
-    const [notice,setNotice] = useState("")
-    const [calander, setCalander] = useState(new Date())
-    
-    const {_id}=useParams();
-    const navigate=useNavigate();
+  const [faculty, setFaculty] = useState([]);
+  const [date, setDate] = useState("");
+  const [topic, setTopic] = useState("");
+  const [notice, setNotice] = useState("");
+  const [calander, setCalander] = useState(new Date());
 
-    useEffect(()=>{
-        if(_id){
-            NoticeService.getNoticeById(_id).then(response=>{
-                setTopic(response.notices.topic);
-                setFaculty(response.notices.faculty);
-                setDate(response.notices.date);
-                setNotice(response.notices.notice);
-            })
-        }
-    },[])
+  const { _id } = useParams();
+  const navigate = useNavigate();
 
-
-    const submitNotice = (e) =>{
-        e.preventDefault();
-        const notices = {faculty,date,topic,notice}
-
-        if (_id) {
-            NoticeService.updateNotice(_id,notices).then((response)=>{
-                navigate("/AdminHome/NoticeTable")
-            })                      
-        }else{
-            NoticeService.createNotice(notices).then((response)=>{
-                Swal.fire(
-                    "Success",
-                    "Notice Added Successfully",
-                    "success"
-                    );
-                navigate("/AdminHome/NoticeTable")
-            }).catch(error =>{
-                console.log(error);
-            })
-        }
+  useEffect(() => {
+    if (_id) {
+      NoticeService.getNoticeById(_id).then((response) => {
+        setTopic(response.notices.topic);
+        setFaculty(response.notices.faculty);
+        setDate(response.notices.date);
+        setNotice(response.notices.notice);
+        console.log(response.notices.notice);
+      });
     }
-    
+  }, []);
+
+  const submitNotice = (e) => {
+    e.preventDefault();
+    const notices = { faculty, date, topic, notice };
+
+    if (_id) {
+      NoticeService.updateNotice(_id, notices).then((response) => {
+        navigate("/AdminHome/NoticeTable");
+      });
+    } else {
+      NoticeService.createNotice(notices)
+        .then((response) => {
+          Swal.fire("Success", "Notice Added Successfully", "success");
+          navigate("/AdminHome/NoticeTable");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  };
+
   return (
     <div>
-       <div className='row'>
-       <div class="card  text-bg-white shadow-lg mb-3 mt-5 text-center" style={{maxWidth:900, marginLeft:180, borderRadius:30}}>  
-            <div class="card-body">
-                <h2 class="card-title mt-1">Add Notice</h2>
-                <form onSubmit={submitNotice}>
-                    <div>
-                        <div className="row w-50  mx-auto mt-5">
-                            <label className="col-sm-3  col-form-label">Faculty</label>
+      <div className="row">
+        <div
+          class="card  text-bg-white shadow-lg mb-3 mt-5 text-center"
+          style={{ maxWidth: 900, marginLeft: 180, borderRadius: 30 }}
+        >
+          <div class="card-body">
+            <h2 class="card-title mt-1">Add Notice</h2>
+            <form onSubmit={submitNotice}>
+              <div>
+                <div className="row w-50  mx-auto mt-5">
+                  <label className="col-sm-3  col-form-label">Faculty</label>
 
-                            <select class="form-select w-75"
-                                    aria-label="Default select example"
-                                    value={faculty}
-                                    required
-                                    placeholder='SelectFaculty..'
-                                    onChange={(e) => {setFaculty(e.target.value);}}>
-                                    <option value="s">Select fac </option>
-                                    <option value="Faculty of Computing">Faculty of Computing</option>
-                                    <option value="Faculty of Business">Faculty of Business</option>
-                                    <option value="Faculty of Engineering">Faculty of Engineering</option>
-                            </select>
-                        </div>
-                        <div className="row w-50  mx-auto mt-3">
-                            <label style={{marginLeft:-9}} className="col-sm-3 col-form-label">Date</label>
-                            <input name="date" 
-                                className="form-control w-75"
-                                placeholder="Add Topic..."
-                                type="date"
-                                value={date}
-                                onChange={(e) => {setDate(e.target.value);}}
-                                style={{marginLeft:9}}
-                                required
-                            />
+                  <select
+                    class="form-select w-75"
+                    aria-label="Default select example"
+                    value={faculty}
+                    required
+                    placeholder="SelectFaculty.."
+                    onChange={(e) => {
+                      setFaculty(e.target.value);
+                    }}
+                  >
+                    <option value="s">Select fac </option>
+                    <option value="Faculty of Computing">
+                      Faculty of Computing
+                    </option>
+                    <option value="Faculty of Business">
+                      Faculty of Business
+                    </option>
+                    <option value="Faculty of Engineering">
+                      Faculty of Engineering
+                    </option>
+                  </select>
+                </div>
+                <div className="row w-50  mx-auto mt-3">
+                  <label
+                    style={{ marginLeft: -9 }}
+                    className="col-sm-3 col-form-label"
+                  >
+                    Date
+                  </label>
+                  <input
+                    name="date"
+                    className="form-control w-75"
+                    placeholder="Add Topic..."
+                    type="date"
+                    value={date}
+                    onChange={(e) => {
+                      setDate(e.target.value);
+                    }}
+                    style={{ marginLeft: 9 }}
+                    required
+                  />
 
-
-                            {/* <select class="form-select w-75" 
+                  {/* <select class="form-select w-75" 
                                     aria-label="Default select example"
                                     value={module.value}
                                     required
@@ -96,61 +114,75 @@ const NoticeForm = () => {
                                     <option value="SE3060-Application Framework">SE3060-Application Framework</option>
                                     <option value="SE3090-Softwre Architecture">SE3090-Softwre Architecture</option>
                             </select> */}
+                </div>
+                <div className="row w-50  mx-auto mt-3">
+                  <label
+                    style={{ marginLeft: -9 }}
+                    className="col-sm-3  col-form-label"
+                  >
+                    Topic
+                  </label>
+                  <input
+                    name="topic"
+                    style={{ marginLeft: 9 }}
+                    className="form-control w-75"
+                    placeholder="Add Topic..."
+                    type="text"
+                    value={topic}
+                    minLength="5"
+                    onChange={(e) => {
+                      setTopic(e.target.value);
+                    }}
+                    required
+                  />
+                </div>
 
-                        </div>
-                        <div className="row w-50  mx-auto mt-3">
-                            <label style={{marginLeft:-9}} className="col-sm-3  col-form-label">Topic</label>
-                            <input name="topic" 
-                            style={{marginLeft:9}}
-                                className="form-control w-75"
-                                placeholder="Add Topic..."
-                                type="text"
-                                value={topic}
-                                minLength="5"
-                                onChange={(e) => {setTopic(e.target.value);}}
-                                required
-                            />
-                        </div>
+                <div className="row w-50  mx-auto mt-3">
+                  <label
+                    style={{ marginLeft: -3 }}
+                    className="col-sm-3  col-form-label"
+                  >
+                    Notice
+                  </label>
 
+                  <textarea
+                    name="notice"
+                    style={{ marginLeft: 3 }}
+                    className="form-control w-75"
+                    placeholder="Add notice...."
+                    type="text"
+                    value={notice}
+                    minLength="6"
+                    onChange={(e) => {
+                      setNotice(e.target.value);
+                    }}
+                    required
+                  />
+                </div>
 
-                        <div className="row w-50  mx-auto mt-3">
-                            <label style={{marginLeft:-3}} className="col-sm-3  col-form-label">Notice</label>
-
-                            <textarea name="notice" 
-                                style={{marginLeft:3}}                            
-                                className="form-control w-75"
-                                placeholder="Add notice...."
-                                type="text"
-                                value={notice}
-                                minLength="6"
-                                onChange={(e) => {setNotice(e.target.value);}}
-                                required
-                            />
-                        </div>
-
-                        <div className="row w-50 mx-auto mt-3 mb-4 " style={{borderRadius:30}}>
-                            <input
-                                className="btn btn-primary mt-4 mx-auto shadow-lg"
-                                type="submit"
-                                value="Save"
-                            />
-                            </div>
-
-                        
-
-
-                        
-                    </div>
-                </form>
-            </div>
-            </div> 
-            <div class="card  text-bg-white shadow-lg mb-3 mt-5 text-center p-4" style={{maxWidth:350, marginLeft:50}}>
-                <Calendar setCalander={setCalander}
-                value={calander}></Calendar></div> 
-            </div>
-                       
+                <div
+                  className="row w-50 mx-auto mt-3 mb-4 "
+                  style={{ borderRadius: 30 }}
+                >
+                  <input
+                    className="btn btn-primary mt-4 mx-auto shadow-lg"
+                    type="submit"
+                    value="Save"
+                  />
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div
+          class="card  text-bg-white shadow-lg mb-3 mt-5 text-center p-4"
+          style={{ maxWidth: 350, marginLeft: 50 }}
+        >
+          <Calendar setCalander={setCalander} value={calander}></Calendar>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NoticeForm
+export default NoticeForm;
--- front-end/src/Components/Student/StudentViewLibraryItem.jsx
+++ front-end/src/Components/Student/StudentViewLibraryItem.jsx
@@ -1,25 +1,90 @@
-import React from 'react'
-import { useEffect } from 'react';
-import { useState } from 'react'
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import "../Lecture/grid.css";
+import { Button, Card } from "react-bootstrap";
 
 function StudentViewLibraryItem() {
-  const[items, setItems] = useState();
+  const [items, setItems] = useState();
 
-  useEffect(()=>{
-    const fetchFilers = async () =>{
+  useEffect(() => {
+    const fetchFilers = async () => {
       const res = await fetch(`http://localhost:5000/pdf`);
       const data = await res.json();
       setItems(data);
     };
     fetchFilers();
-  },[])
+  }, []);
 
   console.log(items);
 
-
   return (
-    <div>StudentViewLibraryItem</div>
-  )
+    <div className="container grid offset-md-1 offset-md-1">
+      {items?.map((row) => (
+        <Card className="m-3 box box-shadow" style={{ width: "18rem" }}>
+          <Card.Header>
+            <div className="row">
+              <div
+                style={{
+                  display: "inline-block",
+                  width: "250px",
+                  fontSize: "20px",
+                }}
+              >
+                Subject:&emsp;{row.subject}
+              </div>
+              <div style={{ display: "inline-block", width: "250px" }}>
+                {row.year}
+              </div>
+            </div>
+          </Card.Header>
+          <Card.Body>
+            <Card.Text>
+              <div className="row">
+                <div className="col-10">
+                  <strong
+                    style={{
+                      display: "inline-block",
+                      width: "100px",
+                    }}
+                  >
+                    faculty:
+                  </strong>
+                  {row.faculty} <br></br>
+                </div>
+
+                <div
+                  className="row "
+                  style={{ display: "flex", justifyContent: "right" }}
+                >
+                  <div className="col-auto">
+                    <Button
+                      className="mx-1"
+                      variant="info"
+                      size="sm"
+                      block
+                      // onClick={() => getLecturesByid(row._id)}
+                    >
+                      More
+                    </Button>
+                    <Button
+                      className="mx-1"
+                      variant="danger"
+                      size="sm"
+                      block
+                      // onClick={() => deleteLecture(row._id)}
+                    >
+                      Delete
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      ))}
+    </div>
+  );
 }
 
-export default StudentViewLibraryItem
+export default StudentViewLibraryItem;
